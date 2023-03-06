f2ff82f9bbbba4b21fe7c7a27fb6332d383f0905
leofab <leofabr3@gmail.com>
2023-03-06 14:13:39 -0300

    List exercise first step

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Double> list = new ArrayList<>();
+        list.add(33.7);
+        list.add(30.2);
+        list.add(29.4);
+        list.add(27.3);
+        list.add(32.1);
+        list.add(29.9);
+        System.out.println(list);
     }
 }

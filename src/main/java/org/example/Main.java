5bd90a88e80ea116960936663c869c8a8a288dff
leofab <leofabr3@gmail.com>
2023-03-06 14:31:23 -0300

    List exercise resolution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,44 @@
 package org.example;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+        DecimalFormat df = new DecimalFormat("0.00");
+
         ArrayList<Double> list = new ArrayList<>();
-        list.add(33.7);
-        list.add(30.2);
-        list.add(29.4);
-        list.add(27.3);
-        list.add(32.1);
-        list.add(29.9);
+        ArrayList<String> meses = new ArrayList<>();
+
+        list.add(33.71);
+        list.add(30.24);
+        list.add(29.43);
+        list.add(27.31);
+        list.add(32.19);
+        list.add(29.95);
+
+        meses.add("Janeiro");
+        meses.add("Fevereiro");
+        meses.add("Março");
+        meses.add("Abril");
+        meses.add("Maio");
+        meses.add("Junho");
+
         System.out.println(list);
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
+
+        double somaTemp = list.stream().mapToDouble(Double::doubleValue).sum();
+        double media = somaTemp / list.size();
+
+        System.out.println(df.format(somaTemp));
+        System.out.println(df.format(media));
+
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i) > media){
+                System.out.println("No mês de " + meses.get(i) + " tivemos uma temperatura maior que a média dos seis meses.");
+            }
+        }
     }
 }
